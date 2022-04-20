74a697d8406eb6bbb13b6e4c2c1bcb414789f657
nisargpatel1504 <nisargpatel1504@gmail.com>
2022-04-20 12:04:49 -0400

    sitter profile

--- lib/screens/notification_cart.dart
+++ lib/screens/notification_cart.dart
@@ -12,42 +12,93 @@ class SitterCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.album),
-              title: Text(giftItem.name.toString()),
-              subtitle: Text(giftItem.email.toString()),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                IconButton(
-                  onPressed: () {
-                    GiftManager().update_data(giftItem.id.toString());
-                  },
-                  icon: Icon(
-                    Icons.check_circle,
-                    color: Colors.black.withOpacity(0.5),
-                    size: 18,
+      child:Column(
+        children: [
+          if(giftItem.status.toString() == "Pending")...[
+            new Card(
+
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.album),
+                    title: Text(giftItem.name.toString()),
+                    subtitle: Text(giftItem.email.toString()),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.cancel,
-                    color: Colors.black.withOpacity(0.5),
-                    size: 18,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () {
+                          GiftManager().update_data(giftItem.id.toString());
+                        },
+                        icon: Icon(
+                          Icons.check_circle,
+                          color: Colors.black.withOpacity(0.5),
+                          size: 18,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.cancel,
+                          color: Colors.black.withOpacity(0.5),
+                          size: 18,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                    ],
                   ),
-                ),
-                const SizedBox(width: 8),
-              ],
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
+          ]
+          else...[
+            Card(
+
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.album),
+                    title: Text(giftItem.name.toString()),
+                    subtitle: Text(giftItem.email.toString()),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      // IconButton(
+                      //   onPressed: () {
+                      //     GiftManager().update_data(giftItem.id.toString());
+                      //   },
+                      //   icon: Icon(
+                      //     Icons.check_circle,
+                      //     color: Colors.black.withOpacity(0.5),
+                      //     size: 18,
+                      //   ),
+                      // ),
+                      // IconButton(
+                      //   onPressed: () {},
+                      //   icon: Icon(
+                      //     Icons.cancel,
+                      //     color: Colors.black.withOpacity(0.5),
+                      //     size: 18,
+                      //   ),
+                      // ),
+                      const SizedBox(width: 8),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ]
+        ],
+      )
+
+
     );
+
+
+    }
   }
-}
+
